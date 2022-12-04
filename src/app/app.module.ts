44c63d13bdf770634b3de0ee6aa5519639f789cd
sagar <sagar43550@gmail.com>
2022-12-04 22:43:20 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from 'src/core/core.module';
+import { MaterialModule } from './module.component';
+import { MidContainerComponent } from './mid-container/mid-container.component';
+import { BottomContainerComponent } from './bottom-container/bottom-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MidContainerComponent,
+    BottomContainerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    CoreModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mid-container/mid-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mid-container',
+  templateUrl: './mid-container.component.html',
+  styleUrls: ['./mid-container.component.scss']
+})
+export class MidContainerComponent implements OnInit {
+
+  constructor() { }
+ 
+  ngOnInit(): void {
+  }
+
+}
